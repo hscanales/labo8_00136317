8c1d165462259e91d574b9af960ad0a725f0155b
hscanales <00136317@uca.edu.sv>
2018-10-19 11:08:25 -0600

    final

--- asset/js/app.js
+++ asset/js/app.js
@@ -26,3 +26,74 @@ navSlide();
 var cont = 1;
 var bitacoras = [];
 var formulario = document.getElementById("bitacora");
+
+formulario.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    let bitacora = {
+        cant: cont,
+        fecha: formulario[1].value,
+        descripcion: formulario[2].value,
+        cantidad: formulario[3].value
+    }
+    if (bitacora.cantidad == "" || bitacora.fecha == '' || bitacora.descripcion == "") {
+        formulario.style.border = "2px solid red";
+        if (bitacora.cantidad == "") {
+            formulario[3].style.border = "2px solid red";
+        }
+        if (bitacora.fecha == '') {
+            formulario[1].style.border = "2px solid red";
+        }
+        if (bitacora.descripcion == "") {
+            formulario[2].style.border = "2px solid red";
+        }
+    } else {
+        bitacoras.push(bitacora);
+        formulario.style.border = "2px solid green";
+        formulario[1].style.border = "default";
+        formulario[2].style.border = "default";
+        formulario[3].style.border = "default";
+    };
+    cont++;
+    mostrar();
+});
+
+const crearElemento = (bitacora, tbody) => {
+    let tr = document.createElement("tr");
+    Object.values(bitacora).forEach(item => {
+        let td = document.createElement("td");
+        let content = document.createTextNode(item);
+        td.appendChild(content);
+        tr.setAttribute("class", "click");
+        tr.appendChild(td);
+    });
+
+    tbody.appendChild(tr);
+}
+
+const eliminar = (tbody) => {
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+
+};
+
+const agregar = () => {
+    var eventtr = document.querySelectorAll(".click");
+    eventtr.forEach((item, index) => {
+        item.addEventListener("click", () => {
+            document.querySelector("#fecha").value = item.childNodes[1].textContent;
+            document.querySelector("#descp").value = item.childNodes[2].textContent;
+            document.querySelector("#cant").value = item.childNodes[3].textContent;
+        });
+    })
+};
+
+const mostrar = () => {
+    if (document.querySelector(".tabla-btc tbody").childElementCount > 0) {
+        eliminar(document.querySelector(".tabla-btc tbody"));
+    }
+    bitacoras.forEach(item => {
+        crearElemento(item, document.querySelector(".tabla-btc tbody"));
+    });
+    agregar();
+};
---
+++ readme.md
@@ -0,0 +1,15 @@
+Respuestas: 
+1- Formulario contiene el elemento form de bitacora
+2- El metodo preventDefault() hace que se evite la secuencia que tiene que desarrollar el evento por defecto.
+3- Dentro de Formulario[x] se encuentra los valores que el usuario mete en las diferentes labels de el form.
+4- td tiene a content del formulario ingresados por el usuario y tr tiene la "lista" de los diferentes datos que ingreso el usuario;
+5- content tiene el dato exacto que ingreso el usuario en el formulario
+6- tendra la bitacora
+7- inserta en la tabla la bitacora ingresada por el usuario
+8- FirstChild retorna el primer nodo del arbol
+9- Vacio
+10- seteamos el dato dentro de la lista de la bitacora
+11- retorna el nodo de la tabla
+12- cuenta los nodos hijos
+13- 
+14- Se mostrara agregado la bitacora en el nodo
